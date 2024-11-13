f061aad67afb8b26f116a63828ee0d678ca6729f
im-sanny <raselparvezsanny@gmail.com>
2024-11-13 09:04:02 +0600

    create: created besic server and connected to mongodb

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 9000;
+
+const app = express();
+
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.lwjqs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello form job portal....");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
